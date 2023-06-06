3ffac9ba849b90c07b1725c9ac2b9cd4333d92d6
carlosmoshudev <carlos@carlosmoshu.com>
2023-06-06 11:18:11 +0200

    test

--- lse-translator/tests/test_translator.py
+++ lse-translator/tests/test_translator.py
@@ -3,9 +3,15 @@ import src.translator as Translator
 
 class TestTranslator(unittest.TestCase):
     def setUp(self):
-        self.translator = Translator()
+        self.translator = Translator('../../config.json')
 
-    def test_translate(self):
+    def test_translate_toe_string(self):
+        input_text = "Hola, estoy haciendo un test de mi función translate, para ver que me respondes un texto."
+
+        output = self.translator.translate(input_text)
+        self.assertIsInstance(output, str)
+
+    def test_translate_tobe_tokenizable(self):
         input_text = "buenos días, hoy voy a ir a trabajar en coche"
         expected_output = "buenos días hoy yo trabajar coche ir"
 
